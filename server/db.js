ba71049c4ae4c472184d16662b090f06289b415d
Nick Lorusso <ndlorusso@gmail.com>
2024-05-08 12:14:14 -0500

    createUser, createProduct, and hash password

--- server/db.js
+++ server/db.js
@@ -1,6 +1,9 @@
 const pg = require('pg');
 const client = new pg.Client(process.env.DATABASE_URL || "postgres://localhost/acme_store_35");
 
+const uuid = require('uuid');
+const bcrypt = require('bcrypt');
+
 const createTables = async () => {
     const SQL = `--sql
     DROP TABLE IF EXISTS favorites;
@@ -28,15 +31,50 @@ const createTables = async () => {
     await client.query(SQL);
 };
 
+const createUser = async ({ username, password }) => {
+    const SQL = `--sql
+    INSERT INTO users(id, username, password)
+    VALUES($1, $2, $3)
+    RETURNING *
+    `;
+    const response = await client.query(SQL, [uuid.v4(), username, await bcrypt.hash(password, 5)]);
+    return response.rows[0];
+};
 
+
+const createProduct = async ({name}) => {
+    const SQL = `--sql
+    INSERT INTO products(id, name)
+    VALUES($1, $2)
+    RETURNING *
+    `;
+    const response = await client.query(SQL, [uuid.v4(), name]);
+    return response.rows[0];
+};
+
+const fetchUsers = async () => {
+    const SQL = `--sql
+    SELECT * FROM users;
+    `;
+    const response = await client.query(SQL);
+    return response.rows;
+};
+
+const fetchProducts = async () => {
+    const SQL = `--sql
+    SELECT * FROM products;
+    `;
+    const response = await client.query(SQL);
+    return response.rows;
+};
 
 module.exports = {
     client,
     createTables,
-//     createProduct,
-//     createUser,
-//     fetchUsers,
-//     fetchProducts,
+    createUser,
+    createProduct,
+    fetchUsers,
+    fetchProducts,
 //     createFavorite,
 //     fetchFavorites,
 //     deleteFavorite,
--- server/index.js
+++ server/index.js
@@ -18,6 +18,24 @@ const init = async () => {
     await client.connect();
     await createTables();
     console.log('created tables');
+
+    const [nick, sophia, katherine, electronics, books, cooking] = await Promise.all([
+        createUser({username: 'nick', password: 'secret'}),
+        createUser({username: 'sophia', password: 'secret1'}),
+        createUser({username: 'katherine', password: 'secret2'}),
+        createProduct({name: 'electronics'}),
+        createProduct({name: 'books'}),
+        createProduct({name: 'cooking'})
+    ]);
+
+    const users = await fetchUsers();
+    console.log(users);
+    const products = await fetchProducts();
+    console.log(products);
+
+    // console.log(nick.id);
+    // console.log(electronics.id);
+
     app.listen(port, () => console.log(`listening on port ${port}`));
 
 };
